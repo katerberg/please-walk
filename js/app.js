1d3495ce052385d10dce0e830861a5c45e86e17b
katerberg <katerberg@fastmail.fm>
2015-03-12 22:09:53 -0500

    Adds basic keyboard

--- js/app.js
+++ js/app.js
@@ -1,50 +1,133 @@
 (function() {
-$(document).ready(function() {
-var gameState = {
-    boardSize: 400,
-    ballSize: 20,
-    frameRate: 10,
-    velocity: {
-        x: 1,
-        y: 1
-    },
-    position: {
-        x: 41,
-        y: 1
+var PLEASEWALK = {};
+
+PLEASEWALK.ball = function() {
+    var size = 20,
+        movementStrength = 1,
+        velocity = {
+            x: 3,
+            y: 3
+        },
+        position = {
+            x: 41,
+            y: 21
+        },
+        moving = {
+            LEFT: false,
+            RIGHT: false,
+            UP: false,
+            DOWN: false
+        };
+    function isBounded(distance) {
+        return (PLEASEWALK.boardSize < (distance + size) || distance <= size)
     }
+
+    function drawBall(context) {
+        context.beginPath();
+        context.arc(position.x, position.y, size, 0, 2 * Math.PI, false);
+        context.fillStyle = 'tomato';
+        context.fill();
+        context.stroke();
+    }
+
+    function advance() {
+        position.x += velocity.x;
+        position.y += velocity.y;
+        if (moving.LEFT) {
+            position.x -= movementStrength;
+        }
+        if (moving.RIGHT) {
+            position.x += movementStrength;
+        }
+        if (moving.UP) {
+            position.y -= movementStrength;
+        }
+        if (moving.DOWN) {
+            position.y += movementStrength;
+        }
+        if (isBounded(position.x)) {
+            velocity.x *= -1
+        }
+        if (isBounded(position.y)) {
+            velocity.y *= -1
+        }
+    }
+
+    function startMoving(direction) {
+        moving[direction] = true;
+    }
+
+    function stopMoving(direction) {
+        moving[direction] = false;
+    }
+
+    return {
+        draw: drawBall,
+        advance: advance,
+        startMoving: startMoving,
+        stopMoving: stopMoving
+    };
 };
-var context;
 
 
-function init() {
-    var canvas = $('#canvas');
-    canvas.attr('width', gameState.boardSize);
-    canvas.attr('height', gameState.boardSize);
-    context = canvas[0].getContext('2d');
-        
-    context.fillStyle = 'tomato';
+PLEASEWALK.game = (function() {
+    PLEASEWALK.boardSize = 400;
+    PLEASEWALK.frameRate = 10;
+    var keyDirections = {
+        37: 'LEFT',
+        38: 'UP',
+        39: 'RIGHT',
+        40: 'DOWN',
+    },
+    context,
+    ball;
 
-    gameLoop();
-}
+    function init() {
+        var canvas = $('#canvas');
+        canvas.attr('width', PLEASEWALK.boardSize);
+        canvas.attr('height', PLEASEWALK.boardSize);
+        context = canvas[0].getContext('2d');
+        ball = PLEASEWALK.ball();
+        bindEvents();
 
-function isBounded(position) {
-    return (gameState.boardSize < (position + gameState.ballSize) || position <= 0)
-}
-
-function gameLoop() {
-    context.clearRect(0,0, gameState.boardSize, gameState.boardSize);
-    context.fillRect(gameState.position.x, gameState.position.y, gameState.ballSize, gameState.ballSize);
-    gameState.position.x = gameState.position.x + gameState.velocity.x;
-    gameState.position.y = gameState.position.y + gameState.velocity.y;
-    if (isBounded(gameState.position.x)) {
-        gameState.velocity.x *= -1
+        gameLoop();
     }
-    if (isBounded(gameState.position.y)) {
-        gameState.velocity.y *= -1
-    }
-    setTimeout(gameLoop, gameState.frameRate);
-}
 
-init();
+    function clearScreen() {
+        context.clearRect(0, 0, PLEASEWALK.boardSize, PLEASEWALK.boardSize);
+    }
+
+    function bindEvents() {
+        $(document).keydown(function (event) {
+            var key = event.which;
+            var direction = keyDirections[key];
+            if (direction) {
+                ball.startMoving(direction);
+                event.preventDefault();
+            }
+        });
+        $(document).keyup(function (event) {
+            var key = event.which;
+            var direction = keyDirections[key];
+            if (direction) {
+                ball.stopMoving(direction);
+                event.preventDefault();
+            }
+        });
+    }
+
+
+    function gameLoop() {
+        clearScreen();
+        ball.draw(context);
+        ball.advance();
+        setTimeout(gameLoop, PLEASEWALK.frameRate);
+    }
+
+    return {init: init};
+})();
+
+$(document).ready(function() {
+    PLEASEWALK.game.init();
 });
 })();
