c29212a951b435af5ebdf5de78897acef6d6fa16
katerberg <katerberg@fastmail.fm>
2015-03-30 21:56:00 -0500

    Adds winning and score functionality

--- js/app.js
+++ js/app.js
@@ -1,8 +1,46 @@
 (function() {
 var PLEASEWALK = {};
 
+PLEASEWALK.winTarget = function() {
+    coordinates = {}
+
+    function getSize(baseSize) {
+        var maxSize = baseSize * 0.1;
+        var minSize = baseSize * 0.05;
+        return Math.floor(Math.random() * maxSize + minSize);
+    }
+
+    function getPosition(size) {
+        return Math.floor(Math.random() * size);
+    }
+
+    function init() {
+        coordinates.width = getSize(PLEASEWALK.boardSize.x);
+        coordinates.height = getSize(PLEASEWALK.boardSize.y);
+        coordinates.x = getPosition(PLEASEWALK.boardSize.x - coordinates.width)
+        coordinates.y = getPosition(PLEASEWALK.boardSize.y - coordinates.height)
+    }
+
+    function draw(context) {
+        var color = '#70AF00';
+        context.beginPath();
+        context.strokeStyle = color;
+        context.fillStyle = color;
+        context.strokeRect(coordinates.x, coordinates.y, coordinates.width, coordinates.height);
+        context.fillRect(coordinates.x, coordinates.y, coordinates.width, coordinates.height);
+        context.closePath();
+    }
+
+    init()
+
+    return {
+        coordinates: coordinates,
+        draw: draw
+    };
+}
+
 PLEASEWALK.character = function() {
-    var size = 20,
+    var radius = 20,
         movementStrength = 1,
         position = {
             x: 41,
@@ -15,20 +53,47 @@ PLEASEWALK.character = function() {
             DOWN: false
         };
 
-    function drawBall(context) {
+    function drawCharacter(context) {
+        var color = 'tomato';
         context.beginPath();
-        context.arc(position.x, position.y, size, 0, 2 * Math.PI, false);
-        context.fillStyle = 'tomato';
-        context.fill();
+        context.strokeStyle = color;
+        context.fillStyle = color;
+        context.arc(position.x, position.y, radius, 0, 2 * Math.PI, false);
         context.stroke();
+        context.closePath();
+        context.fill();
     }
+
     function handleBounding(direction) {
-        if (position[direction] < size) {
+        if (position[direction] < radius) {
             position[direction] += movementStrength;
         }
-        if (position[direction] > (PLEASEWALK.boardSize[direction] - size)) {
+        if (position[direction] > (PLEASEWALK.boardSize[direction] - radius)) {
             position[direction] -= movementStrength;
         }
+    }
+
+    function isInside(x, y, width, height) {
+        var distX = Math.abs(position.x - x - width / 2);
+        var distY = Math.abs(position.y - y - height / 2);
+
+        if (distX > (width / 2 + radius)) {
+            return false;
+        }
+        if (distY > (height / 2 + radius)) {
+            return false;
+        }
+
+        if (distX <= (width / 2)) {
+            return true;
+        }
+        if (distY <= (height / 2)) {
+            return true;
+        }
+
+        var dx = distX - width / 2;
+        var dy = distY - height / 2;
+        return (dx * dx + dy * dy <= (radius * radius));
     }
 
     function advance() {
@@ -57,10 +122,11 @@ PLEASEWALK.character = function() {
     }
 
     return {
-        draw: drawBall,
+        draw: drawCharacter,
         advance: advance,
         startMoving: startMoving,
-        stopMoving: stopMoving
+        stopMoving: stopMoving,
+        isInside: isInside
     };
 };
 
@@ -75,21 +141,33 @@ PLEASEWALK.game = (function() {
         40: 'DOWN',
     },
     context,
-    character;
+    score,
+    character,
+    winTarget;
 
     function init() {
         var canvas = $('#canvas');
+        score = 0;
+        $('#score').text(score)
         canvas.attr('width', PLEASEWALK.boardSize.x);
         canvas.attr('height', PLEASEWALK.boardSize.y);
         context = canvas[0].getContext('2d');
         character = PLEASEWALK.character();
+        winTarget = PLEASEWALK.winTarget()
         bindEvents();
 
         gameLoop();
     }
 
+    function checkWin() {
+        if (character.isInside(winTarget.coordinates.x, winTarget.coordinates.y, winTarget.coordinates.width, winTarget.coordinates.height)) {
+            $('#score').text(++score)
+            winTarget = PLEASEWALK.winTarget();
+        }
+    }
+
     function clearScreen() {
-        context.clearRect(0, 0, PLEASEWALK.boardSize.y, PLEASEWALK.boardSize.y);
+        context.clearRect(0, 0, PLEASEWALK.boardSize.x, PLEASEWALK.boardSize.y);
     }
 
     function bindEvents() {
@@ -115,7 +193,9 @@ PLEASEWALK.game = (function() {
     function gameLoop() {
         clearScreen();
         character.draw(context);
+        winTarget.draw(context);
         character.advance();
+        checkWin()
         setTimeout(gameLoop, PLEASEWALK.frameRate);
     }
 
